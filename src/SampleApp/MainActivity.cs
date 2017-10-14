bc754de076214fc97fcfbdb10619a5218d9327cd
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-10-14 16:28:58 +0200

    Removed intent service

--- src/SampleApp/MainActivity.cs
+++ src/SampleApp/MainActivity.cs
@@ -15,14 +15,12 @@ namespace SampleApp
     {
         private EditText _numInput;
         private Button _btnSetBadge;
-        private Button _launchNotification;
         private Button _removeBadgeBtn;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
             _numInput = FindViewById<EditText>(Resource.Id.numInput);
@@ -30,14 +28,11 @@ namespace SampleApp
             _btnSetBadge = FindViewById<Button>(Resource.Id.btnSetBadge);
             _btnSetBadge.Click += Button_Click;
 
-            _launchNotification = FindViewById<Button>(Resource.Id.btnSetBadgeByNotification);
-            _launchNotification.Click += LaunchNotification_Click;
-
             _removeBadgeBtn = FindViewById<Button>(Resource.Id.btnRemoveBadge);
             _removeBadgeBtn.Click += RemoveBadgeBtn_Click;
 
             //find the home launcher Package
-            Intent intent = new Intent(Intent.ActionMain);
+            var intent = new Intent(Intent.ActionMain);
             intent.AddCategory(Intent.CategoryHome);
             ResolveInfo resolveInfo = PackageManager.ResolveActivity(intent, PackageInfoFlags.MatchDefaultOnly);
             var currentHomePackage = resolveInfo.ActivityInfo.PackageName;
@@ -52,23 +47,6 @@ namespace SampleApp
 
             Toast.MakeText(ApplicationContext, "success=" + success, ToastLength.Short)
                 .Show();
-        }
-
-        private void LaunchNotification_Click(object sender, System.EventArgs e)
-        {
-            if (int.TryParse(_numInput.Text, out int badgeCount))
-            {
-                Finish();
-
-                var intent = new Intent(this, typeof(MainActivity));
-                intent.PutExtra("badgeCount", badgeCount);
-                StartService(intent);
-            }
-            else
-            {
-                Toast.MakeText(ApplicationContext, "Error input", ToastLength.Short)
-                    .Show();
-            }
         }
 
         private void Button_Click(object sender, System.EventArgs e)
